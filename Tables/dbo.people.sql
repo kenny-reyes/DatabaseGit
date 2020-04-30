13dfc3fd6204e2d4c8e9442898a263034c5e8730
Kenny Reyes <kenny.reyesborrueco@altran.com>
2020-04-30 20:58:14 +0200

    second changes

--- Tables/dbo.people.sql
+++ Tables/dbo.people.sql
@@ -2,7 +2,8 @@
   [id] [int] NOT NULL,
   [name] [nchar](30) NOT NULL,
   [surname] [nchar](50) NOT NULL,
-  [surname2] [nchar](50) NULL
+  [surname2] [nchar](50) NULL,
+  [age] [numeric](3) NULL
 )
 ON [PRIMARY]
 GO
